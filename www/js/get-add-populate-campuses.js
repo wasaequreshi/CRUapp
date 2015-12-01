67f381b312a9e9c5784fbe8583fa99cbb707f97c
Wasae Qureshi <wasaequreshi@gmail.com>
2015-12-01 02:08:36 -0800

    Populated campuses and ministries from db. Also fixed how there was no top bar for an event

---
+++ www/js/get-add-populate-campuses.js
@@ -0,0 +1,20 @@
+var eventsData = "test";
+var url = "http://localhost:8080/campuses";
+console.log("Start");
+
+$.ajax({
+   url: url,
+   type: "GET",
+    dataType: "json",
+   success: function (data)
+    {
+        eventsData = data;
+        console.log("In callback");
+        console.log(data);
+        jQuery.each(data, function( key, value ) {
+            console.log(key);
+            console.log(value["name"]);
+            $("#list_of_campuses").append("<li id=\"" + key + "\"><a href=\"selectMinistry.html\">" + value["name"] + "</a></li>");
+        }); 
+    }
+});
