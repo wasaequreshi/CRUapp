704e4b4b36148e6d08f20e5038afc29f0e883151
connor <connorkrier@gmail.com>
2015-12-02 01:22:53 -0800

    made events and summer missions beautiful

--- www/js/template/main.js
+++ www/js/template/main.js
@@ -85,7 +85,7 @@
 				$(
 					'<div id="titleBar">' +
 						'<a href="#header" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
+						'<span class="title">' + $('.active').html() + '</span>' +
 					'</div>'
 				)
 					.appendTo($body);
