19fd505c72cd2be6310d1fa75923b0978b3bf9c5
connor <connorkrier@gmail.com>
2015-12-02 23:50:58 -0800

    added pretty icons

--- www/js/get-and-populate-missions.js
+++ www/js/get-and-populate-missions.js
@@ -7,14 +7,24 @@ $.ajax({
    type: "GET",
    dataType: "json",
    success: function (data)
-   {
-        missionData = data;
+    {
+        eventsData = data;
         console.log("In callback");
         console.log(data);
+        
+        var list = $("#list_of_events");
+        
         jQuery.each(data, function( key, value ) {
             console.log(key);
             console.log(value["name"]);
-            $("#list_of_events").append("<li id=\"" + key + "\"><a href=\"mission-details.html\">" + value["name"] + "</a></li>");
+            var day = value["startDate"];
+            var date = day[8] + day[9];
+            
+            var html = "<span class=\"fa-stack fa-1.5x\"> <i class=\"fa fa-calendar-o fa-stack-2x\" style=\"color: #dd7d1b;\"></i>"
+            + "<strong class=\"fa-stack-1x\" style=\"margin-top: .3em;\">" + date + "</strong> </span>";
+            
+            list.append("<li id=\"" + key + "\" style=\"padding-bottom: 10px; text-align: bottom;\"><a style=\"text-decoration: none;\" href=\"testEvent1.html\">" + html + "<strong style=\"font-size: 1.5em;\">&nbsp; " + value["name"]
+                                        + "</strong></a></li>");
              document.getElementById(key).addEventListener("click", saveData);
         }); 
     }
@@ -25,5 +35,3 @@ function saveData()
    console.log("Hi guys!" + this.id);
    localStorage.setItem('selectedItem', JSON.stringify(missionData[parseInt(this.id, 10)]));
 }
-
-console.log("End")
