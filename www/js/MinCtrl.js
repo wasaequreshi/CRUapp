a8e2ce964afc86ebd8b480c155b363605d21df35
connor <connorkrier@gmail.com>
2016-01-20 17:46:09 -0800

    git made me do it

--- www/js/MinCtrl.js
+++ www/js/MinCtrl.js
@@ -1,34 +1,31 @@
 var min = angular.module('starter.controllers.min', []);
 
-min.controller('MinCtrl', function($scope, $location, $ionicHistory, selectedCampuses) {
-    var url = 'http://54.86.175.74:8080/ministries';
+min.controller('MinCtrl', function($scope, $location, $ionicHistory, $ajax, $localStorage, selectedCampuses, constants) {
+    var url = constants.BASE_SERVER_URL + 'ministries';
     
     //To get the campuses selected by the user on the previous page call: selectedCampuses.getCampuses()
-    
-    $.ajax({
-        url: url,
-        type: "GET",
-        dataType: "json",
-        success: function (data) {
-            //makes the objects "checkable"
-            for (var i = 0; i < data.length; ++i) {
-                data[i].checked = false;
-            }
-           
-            $scope.choices = data;
-        },
-        error: function(xhr, text, err) {
-            //if there is an error (ie 404, 500, etc) redirect to the error page
-            $location.path('/app/error');
+    var success = function (data) {
+        //makes the objects "checkable"
+        for (var i = 0; i < data.length; ++i) {
+            data[i].checked = false;
         }
-    });
+
+        $scope.choices = data;
+    };
+    
+    var err = function(xhr, text, err) {
+        //if there is an error (ie 404, 500, etc) redirect to the error page
+        $location.path('/app/error');
+    };
+    
+    $ajax.get(url, 'json', success, err);
     
     $scope.title = "Select Ministries";
     $scope.next = "Start Using App!";
     
     $scope.goToNext = function() {
         // add campuses and ministries to phone local storage
-        cordova.file.applicationStorageDirectory.setItem();
+        
         $location.path('/app');
         $ionicHistory.nextViewOptions({
             disableAnimate: false,
--- www/js/app.js
+++ www/js/app.js
@@ -100,5 +100,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
   
 // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlist');
+  $urlRouterProvider.otherwise('/app/playlists');
 });
