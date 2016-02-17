0d2f9773fdab7fb46f41eeb59d94452f3a2df632
connor <connorkrier@gmail.com>
2016-02-16 16:48:59 -0800

    further removed bootstrap

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 
-angular.module('starter', ['ionic', 'starter.controllers', 'ui.bootstrap', 'ngCordova'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
 
 .run(function($ionicPlatform) {
