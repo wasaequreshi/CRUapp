df698494908212f7fcdb9cc09ebda74d887a8057
CodyWSears <codywsears@yahoo.com>
2016-03-08 15:10:16 -0800

    Ministream Team Controller updates

--- www/js/controller/TeamCtrl.js
+++ www/js/controller/TeamCtrl.js
@@ -1,8 +1,9 @@
 var teams = angular.module('TeamCtrl', []);
 
-teams.controller('TeamCtrl', function($scope, $location, req, constants, convenience) {
+teams.controller('TeamCtrl', function($scope, $location, req, constants, convenience, $localStorage) {
 	var url = constants.BASE_SERVER_URL + 'ministryteam/list';
-	
+	var mins = $localStorage.getObject(constants.CAMPUSES_CONFIG).ministries;
+    
 	var success = function(data) {
 		$scope.teams = data.data;
 	};
@@ -10,7 +11,23 @@ teams.controller('TeamCtrl', function($scope, $location, req, constants, conveni
 	var err = convenience.defaultErrorCallback('TeamsCtrl',
 		'Could not retrieve list of minstry teams from server');
 
-	req.get(url, success, err);
+    if (mins === '' || mins === [] || typeof mins === 'undefined') {
+        req.get(url, success, err);
+    }
+    else {
+        url = constants.BASE_SERVER_URL + 'ministryteam/find';
+        var minsIds = [];
+        for (var i = 0; i < mins.length; i++) {
+            minsIds.push(mins[i]._id);
+        }
+        
+        var queryParam = {
+            parentMinistry: {$in: minsIds}
+        };
+        
+        //query for teams only in the ministry the user is subscribed to
+        req.post(url, queryParam, success, err);
+    }
 
 	$scope.viewDetails = function(id) {
 		$location.path('/app/teams/' + id);
@@ -20,17 +37,48 @@ teams.controller('TeamCtrl', function($scope, $location, req, constants, conveni
 .controller('TeamDetailCtrl', function($scope, $stateParams, constants, req, convenience) {
     var teamId = $stateParams.id;
     
-    var success = function(data) {
-        var team = data.data;
-        
-        $scope.team = {
-            name: team.name,
-            description: team.description
-        };
-    };
-    
     var err = convenience.defaultErrorCallback('TeamDetailCtrl',
 		'Could not retrieve the selected minstry team from the server');
+    
+    var minUrl = constants.BASE_SERVER_URL + 'ministry/';
+    var minSuccess = function(data) {
+        var min = data.data;
+        $scope.min = min.name;
+    };
+    
+    var leaderUrl = constants.BASE_SERVER_URL + 'user/';
+    var leaderSuccess = function(data) {
+        var leader = data.data;
+        $scope.leader = leader.name;
+    };
+    
+    var success = function(data) {
+        var team = data.data;
+        var image = team.leadersImage;
+        
+        //no leader image
+        if (!image) {
+            //no leader image (DB has two values for the same thing)
+            if (!team.image) {
+                image = constants.PERSON_IMAGE;
+            }
+            else {
+                image = team.image.url;
+            }
+        }
+        else {
+            image = image.url;
+        }
+            
+        $scope.team = {
+            name: team.name,
+            description: team.description,
+            url: image
+        };
+        
+        minUrl += team.parentMinistry;
+        req.get(minUrl, minSuccess, err);
+    };
     
     var url = constants.BASE_SERVER_URL + 'ministryteam/' + teamId;
     
